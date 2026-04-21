6eec39e61027e8b1d6dd475ac999117b23a8cde8
Paweł Oświeciński <wilsonek2001@gmail.com>
2026-04-22 01:47:20 +0200

    Add stub graphql endpoints

---
+++ backend/SoapChat.Api/Features/Mutation/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace SoapChat.Api.Features.Mutation;
+
+[MutationType]
+public static class Account
+{
+    public static bool Login(string username, string password)
+    {
+        return true;
+    }
+}
---
+++ backend/SoapChat.Api/Features/Query/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SoapChat.Api.Features.Query;
+
+public record User(string Id, string Username);
+
+[QueryType]
+public static class Query
+{
+    public static User GetUser() => new User("stub", "stub");
+}
